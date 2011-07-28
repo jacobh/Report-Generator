bb07669fae2b9e179105f39fd31272c3153365bc
charliesome <charlie@charliesomerville.com>
2011-07-28 21:55:46 +1000

    assets

--- app/lib/init.php
+++ app/lib/init.php
@@ -5,11 +5,16 @@ session_start();
 require_once 'config.php';
 date_default_timezone_set($config['timezone']);
 
+error_reporting(E_ALL ^ E_NOTICE);
+
 require_once 'helpers.php';
 
+foreach(glob("lib/deps/*.php") as $dep)
+	require $dep;
+	
 require_once 'models/base.php';
 foreach(glob("lib/models/*.php") as $filter)
 	require_once $filter;
-
+	
 foreach(glob("lib/filters/*.php") as $filter)
 	require $filter;
