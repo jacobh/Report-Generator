4fcb861dd623865f7c4f72b7c0cba786f57d369a
charliesome <charlie@charliesomerville.com>
2011-07-31 16:48:01 +1000

    sketch plan pdf page

---
+++ app/views/pdf/6.php
@@ -0,0 +1,11 @@
+<?php
+
+$pdf->writeHTMLCell(0, 0, '', '', '<table width="100%">
+	<tr>
+		<td width="80%"><span style="color:rgb(0, 167, 143); font-size:18pt; font-weight:bold;"><br/><br/>Sketch Plan</span></td>
+		<td><img src="assets/logo.png" /></td>
+	</tr>
+</table>
+<hr />', 0, 1, 0, true, '', true);
+$pdf->Ln(4);
+$pdf->writeHTMLCell(0, 0, '', '', '<img src="upload/' . $data['sketch'] . '" />', 0, 1, 0, true, '', true);
