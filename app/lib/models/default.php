7f21161bf5654c3946074d5c5af15e5136d8ef5b
charliesome <charlie@charliesomerville.com>
2011-08-16 21:16:40 +1000

    default model json data field

--- app/lib/models/default.php
+++ app/lib/models/default.php
@@ -1,10 +1,14 @@
 <?php
 
 class _Default extends ModelBase {
+	protected $on_save = array("data" => "json_encode");
+	protected $on_restore = array("data" => "legit_json_decode");
+
 	public static $sections = array();
 	public static function load($section) {
 		if(!isset($sections[$section])) {
-			$sections[$section] = self::find_by_section($section);
+			$m = _Default::find_by_section($section)->data;
+			$sections[$section] = $m;
 		}
 		return $sections[$section];
 	}
